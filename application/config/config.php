fd4fc24fe4344934f6a679d35817079311f5b0c4
alfiantaufani <alfiantaufani@gmail.com>
2022-08-18 14:08:49 +0700

    ganti config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/imt-fiks/';
+$config['base_url'] = 'http://localhost/Sistem-IMT/';
 
 /*
 |--------------------------------------------------------------------------
