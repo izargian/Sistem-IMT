768c892c5fad94a5f53e79a566d415ba3065fe76
alfiantaufani <alfiantaufani@gmail.com>
2022-08-25 00:26:23 +0700

    add Access-Control-Allow-Origin

--- application/controllers/api/android/Profil.php
+++ application/controllers/api/android/Profil.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Profil extends CI_Controller
