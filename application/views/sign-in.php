91ea65a4078637764e608460065e348b8acd8a6c
izargian <izargian@gmail.com>
2022-08-26 19:00:11 +0700

    revisi izar sitik

--- application/views/sign-in.php
+++ application/views/sign-in.php
@@ -27,19 +27,19 @@
 		<div class="row">
 			<div class="col-12">
 				<!-- Navbar -->
-				<nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-					<div class="container-fluid">
+				<!-- <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+					<div class="container-fluid"> -->
 						<!-- <a
 								class="navbar-brand font-weight-bolder ms-lg-0 ms-3"
 								href="../pages/dashboard.html"
 							>
 								Soft UI Dashboard
 							</a> -->
-						<button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+						<!-- <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon mt-2">
 								<span class="navbar-toggler-bar bar1"></span>
 								<span class="navbar-toggler-bar bar2"></span>
-								<span class="navbar-toggler-bar bar3"></span>
+								<span class="navbar-toggler-bar bar3"></span> -->
 							</span>
 						</button>
 						<div class="collapse navbar-collapse" id="navigation">
@@ -60,7 +60,7 @@
 											Profile
 										</a>
 									</li> -->
-								<li class="nav-item">
+								<!-- <li class="nav-item">
 									<a class="nav-link me-2" href="#">
 										<i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
 										Sign Up
@@ -71,7 +71,7 @@
 										<i class="fas fa-key opacity-6 text-dark me-1"></i>
 										Sign In
 									</a>
-								</li>
+								</li> -->
 							</ul>
 							<ul class="navbar-nav d-lg-block d-none">
 								<!-- <li class="nav-item">
@@ -98,9 +98,9 @@
 							<div class="card card-plain mt-8">
 								<div class="card-header pb-0 text-left bg-transparent">
 									<h3 class="font-weight-bolder text-info text-gradient">
-										Welcome back
+									Welcome to our website IMT
 									</h3>
-									<p class="mb-0">Enter your email and password to sign in</p>
+									<p class="mb-0">Enter your email and password</p>
 								</div>
 								<div class="card-body">
 									<form role="form" method="post" action="<?= base_url('login') ?>">
@@ -112,10 +112,10 @@
 										<div class="mb-3">
 											<input name="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon" />
 										</div>
-										<div class="form-check form-switch">
+										<!-- <div class="form-check form-switch">
 											<input class="form-check-input" type="checkbox" id="rememberMe" checked="" />
 											<label class="form-check-label" for="rememberMe">Remember me</label>
-										</div>
+										</div> -->
 										<div class="text-center">
 											<button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">
 												Sign in
@@ -123,12 +123,12 @@
 										</div>
 									</form>
 								</div>
-								<div class="card-footer text-center pt-0 px-lg-2 px-1">
+								<!-- <div class="card-footer text-center pt-0 px-lg-2 px-1">
 									<p class="mb-4 text-sm mx-auto">
 										Don't have an account?
 										<a href="javascript:;" class="text-info text-gradient font-weight-bold">Sign up</a>
 									</p>
-								</div>
+								</div> -->
 							</div>
 						</div>
 						<div class="col-md-6">
@@ -144,7 +144,7 @@
 		</section>
 	</main>
 	<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
-	<footer class="footer py-5">
+	<!-- <footer class="footer py-5">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-8 mb-4 mx-auto text-center">
@@ -197,7 +197,7 @@
 				</div>
 			</div>
 		</div>
-	</footer>
+	</footer> -->
 	<!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
 	<!--   Core JS Files   -->
 	<script src="<?= base_url('assets/') ?>js/core/popper.min.js"></script>
